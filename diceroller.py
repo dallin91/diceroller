f7a26f699aba3a436efd80ff8684aafc5c5456ec
dallin91 <dallinreeves@gmail.com>
2019-04-08 14:20:21 -0500

    modified file

--- diceroller.py
+++ diceroller.py
@@ -2,3 +2,16 @@
 # Creator: Dallin Reeves
 # April 8, 2019 1:57PM
 
+import random
+min = 1
+max = 6
+
+roll = input('Would you like to roll? ')
+
+while roll.lower() == 'yes' or roll.lower() == 'y':
+    print('Rolling your lucky number.......')
+    print('You rolled ', random.randint(min, max), '!')
+
+    roll = input('Would you like to roll again? ')
+
+print("Thanks for playing!")
